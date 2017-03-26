941fdafc31a39e74a1181253b642bd083fa0cca2
Milo Turner <iitalics@gmail.com>
2017-03-26 14:49:21 +0000

    simple line plot

--- static/plots.js
+++ static/plots.js
@@ -125,13 +125,69 @@
   }
 
 
-  function Plot2D(selStr, key) {
+  function Plot2D(selStr, targKey, options) {
     let obj = {}
     let sel = d3.select(selStr)
     let width = +sel.attr("width")
     let height = +sel.attr("height")
 
+    options = options || {}
+    options.color = options.color || "#f00";
+    options.color2 = options.color2 || d3.color(options.color).brighter()
+
+    {
+      let midPath = d3.path()
+      midPath.moveTo(0, height / 2)
+      midPath.lineTo(width, height / 2)
+      sel.append("path")
+        .attr("d", midPath)
+        .attr("stroke", "#888")
+    }
+
+    let dataPts = []
+    let maxData = 20
+    let dataGroup = sel.append("g")
+    let dataLine = sel.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "#f44")
+
+    let pad = 10;
+    function posX(i) {
+      return pad + (width - pad*2) * i / (maxData - 1)
+    }
+    function posY(d) {
+      return pad + (height - pad*2) * (d + 1) / 2
+    }
+
+    function drawData() {
+      let upd = dataGroup.selectAll(".dp").data(dataPts)
+
+      let ent = upd.enter()
+        .append("circle")
+          .classed("dp", true)
+          .attr("fill", options.color)
+        .attr("r", 3)
+      upd = upd.merge(ent)
+        .attr("cx", function(d,i) { return posX(i) })
+        .attr("cy", function(d,i) { return posY(d) })
+
+      let path = d3.path()
+      for (let i = 0; i < dataPts.length; i++) {
+        if (i === 0)
+          path.moveTo(posX(i), posY(dataPts[i]))
+        else
+          path.lineTo(posX(i), posY(dataPts[i]))
+      }
+      dataLine.attr("d", path)
+    }
+
     obj.data = function(key, val) {
+      if (key === targKey) {
+        dataPts.push(val)
+        if (dataPts.length > maxData)
+          dataPts.shift()
+        drawData()
+      }
     }
 
     return obj
@@ -152,7 +208,7 @@
       }
       plot3d.data("pos", pos)
       plotX.data("x", pos.x)
-    }, 100)
+    }, 300)
   }, false)
 
 })();
